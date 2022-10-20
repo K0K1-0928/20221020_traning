dbebe5e16085ad28e606257bf68c8089a4b2e5c7
Kouki Murakami <m.kouki.0928@gmail.com>
2022-10-20 18:22:14 +0900

    fix 課題2

--- src/2/conditionalTypes.ts
+++ src/2/conditionalTypes.ts
@@ -1,8 +1,8 @@
 type Rank = `A` | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 'J' | 'Q' | 'K';
 type NotNumber<Type> = Type extends number ? Type : never;
 
-type SpotCard = NotNumber<Rank>;
+type SpotCardRank = NotNumber<Rank>;
 
-const weakestRank: SpotCard = 2;
+const weakestRank: SpotCardRank = 2;
 
 console.log(weakestRank);
